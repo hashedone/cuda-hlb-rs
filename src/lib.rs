3d5d18b0eec4449194c816484d857cbe34aaff9c
Bartłomiej Kuras <hash.deb@gmail.com>
2017-08-24 21:25:36 +0200

    Refactor to more abstract structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,82 @@
-#![allow(dead_code)]
 #![feature(try_trait)]
 #![feature(conservative_impl_trait)]
+#![recursion_limit = "1024"]
 
 extern crate libc;
+#[macro_use]
+extern crate error_chain;
 
 mod ffi;
+pub mod result;
+pub mod stream;
 
-#[cfg(feature = "compiler")]
-pub mod compiler;
+pub use self::result::Result;
+pub use self::stream::Stream;
 
-#[cfg(feature = "runtime")]
-pub mod runtime;
+use std::mem::uninitialized;
+
+static INIT: std::sync::Once = std::sync::ONCE_INIT;
+
+pub fn init() {
+    unsafe { INIT.call_once(|| { ffi::cuInit(0); }) };
+}
+
+enum Context {
+    Primary(ffi::CUdevice, ffi::CUcontext),
+}
+
+pub struct Cuda {
+    context: Context,
+}
+
+impl Cuda {
+    pub fn with_primary_context() -> Result<Cuda> {
+        init();
+        unsafe {
+            let mut devcnt = uninitialized();
+            ffi::cuDeviceGetCount(&mut devcnt)?;
+            if devcnt < 1 {
+                bail!(result::ErrorKind::NoDevices)
+            }
+        }
+        unsafe {
+            let mut device = uninitialized();
+            ffi::cuDeviceGet(&mut device, 0)?;
+            let mut context = uninitialized();
+            ffi::cuDevicePrimaryCtxRetain(&mut context, device)?;
+            let context = Context::Primary(device, context);
+            Ok(Cuda { context })
+        }
+    }
+
+    fn make_current(&self) -> Result<ffi::CUcontext> {
+        let context = match self.context {
+            Context::Primary(_, context) => context,
+        };
+
+        unsafe {
+            ffi::cuCtxSetCurrent(context)?;
+        }
+        Ok(context)
+    }
+
+    pub fn new_stream(&self) -> Result<Stream> {
+        self.make_current()?;
+        Stream::new()
+    }
+
+    pub fn new_non_blocking_stream(&self) -> Result<Stream> {
+        self.make_current()?;
+        Stream::new_non_blocking()
+    }
+}
+
+impl Drop for Cuda {
+    fn drop(&mut self) {
+        match self.context {
+            Context::Primary(device, _) => unsafe {
+                ffi::cuDevicePrimaryCtxRelease(device);
+            },
+        }
+    }
+}
---
+++ src/result.rs
@@ -0,0 +1,51 @@
+#![allow(unused_doc_comment)]
+
+use ffi;
+use std;
+use std::mem::uninitialized;
+
+pub type DriverError = ffi::CUresult;
+
+fn get_cuda_err_string(err: DriverError) -> String {
+    unsafe {
+        let mut buf = uninitialized();
+        match ffi::cuGetErrorString(err, &mut buf) {
+            ffi::CUresult::CUDA_SUCCESS => std::ffi::CStr::from_ptr(buf)
+                .to_str()
+                .map(|s| String::from(s))
+                .unwrap_or_else(|_| String::new()),
+            _ => String::new()
+        }
+    }
+}
+
+error_chain! {
+    errors {
+        Driver(e: DriverError) {
+            display("Driver error: {}", get_cuda_err_string(*e))
+        }
+        NoDevices
+    }
+}
+
+impl std::ops::Try for ffi::CUresult {
+    type Ok = ();
+    type Error = Error;
+
+    fn into_result(self) -> Result<()> {
+        match self {
+            ffi::CUresult::CUDA_SUCCESS => Ok(()),
+            err => Err(ErrorKind::Driver(err).into())
+        }
+    }
+
+    fn from_error(v: Error) -> ffi::CUresult {
+        match v {
+            Error(ErrorKind::Driver(err), _) => err,
+            _ => ffi::CUresult::CUDA_ERROR_UNKNOWN
+        }
+    }
+
+    fn from_ok(_: ()) -> ffi::CUresult { ffi::CUresult::CUDA_SUCCESS }
+}
+
